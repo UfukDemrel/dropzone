3b21e5b877018529a35a77360d6aa4020c9003df
UfukDemrel <126552914+UfukDemrel@users.noreply.github.com>
2024-01-18 17:55:31 +0300

    add

--- src/App.js
+++ src/App.js
@@ -30,31 +30,37 @@ function App() {
   };
 
   return (
-      <div className='app'>
-        <header className='header'>Dropzone</header>
-        <div className='file'>
-          <div className='text-size'>
-            <div className='text'>Send your file by Mail</div>
-            <div className='text-2'>You can make fast transactions by sending your files to the e-mail address you have entered.</div>
-          </div>
-          <form onSubmit={handleSubmit}>
-            <div class="input-group flex-nowrap">
-              <span class="input-group-text" id="addon-wrapping">@</span>
-              <input type="email" value={email} onChange={handleEmailChange} class="form-control" placeholder="E-mail" aria-label="Username" aria-describedby="addon-wrapping" />
-            </div>
-            <br />
-            <div className='dropzone'> 
-            <div className='input-text'>Try it now.</div>
-              <input className="input" type="file" onChange={handleFileChange} />
-            </div>
-            
-            <br />
-            <div className='button'>
-              <button type="button" class="send btn btn-dark">Send Mail</button>
-            </div>
-          </form>
+    <div className='app'>
+      <header className='header'>Dropzone</header>
+      <div className='file'>
+        <div className='text-size'>
+          <div className='text'>Send your file by Mail</div>
+          <div className='text-2'>You can make fast transactions by sending your files to the e-mail address you have entered.</div>
         </div>
+        <form onSubmit={handleSubmit}>
+          <div class="input-group flex-nowrap">
+            <span class="input-group-text" id="addon-wrapping">@</span>
+            <input type="email" value={email} onChange={handleEmailChange} class="form-control" placeholder="E-mail" aria-label="Username" aria-describedby="addon-wrapping" />
+          </div>
+          <br />
+          <div className='dropzone'>
+            <div className='drop-2'>
+              <div className='drop-3'>
+              <h1 className='input-text'>Try it out!</h1>
+              <p className='input-text-2'>Drag and drop files here</p>
+              <p>This is just a demo Dropzone. Dropped files are not actually uploaded.</p>
+              <input className="input" type="file" onChange={handleFileChange} />
+              </div>
+            </div>
+          </div>
+
+          <br />
+          <div className='button'>
+            <button type="button" class="send btn btn-dark">Send Mail</button>
+          </div>
+        </form>
       </div>
+    </div>
   );
 }
 
